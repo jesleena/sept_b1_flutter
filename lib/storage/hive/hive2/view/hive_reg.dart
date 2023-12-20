7da846117bfbeb60ac2d40f14b653adb68777f82
jesle <jesleenaiqbal09@gmail.com>
2023-12-20 11:17:13 +0530

    committ

---
+++ lib/storage/hive/hive2/view/hive_reg.dart
@@ -0,0 +1,94 @@
+import 'package:email_validator/email_validator.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:hive_flutter/adapters.dart';
+import 'package:sept1_batch/storage/hive/hive2/model/user_model.dart';
+import 'package:sept1_batch/storage/hive/hive2/view/hive_login.dart';
+import '../database/hivedb.dart';
+void main()async{
+  WidgetsFlutterBinding.ensureInitialized();
+  Hive.initFlutter();
+  Hive.registerAdapter(UserAdapter());
+  await Hive.openBox<User>('userData');
+  runApp(GetMaterialApp(home: Hive_reg(),));
+}
+class Hive_reg extends StatelessWidget {
+  final emil_ctrl = TextEditingController();
+  final pwd_ctrl = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          TextField(
+            controller: emil_ctrl,
+            decoration: InputDecoration(hintText: "email"),
+          ),
+          TextField(
+            controller: pwd_ctrl,
+            decoration: InputDecoration(hintText: "password"),
+          ),
+          ElevatedButton(onPressed: () async {
+            final regUserList=await HiveDb.instance.getUser();
+            validate_registration(regUserList);
+          },
+
+
+              child: Text("Register Here"))
+        ],
+      ),
+    );
+  }
+
+  void validate_registration(List<User> regUserList) async{
+    final eemail=emil_ctrl.text.trim();
+    final epwd=pwd_ctrl.text.trim();
+    bool userFound=false;
+    final validateEmail=EmailValidator.validate(eemail);
+
+    if(eemail!='' && epwd!='')
+    {
+      if(validateEmail==true){
+            //to validate email
+        await Future.forEach(regUserList, (user) { //to check email already existing
+          if(user.email==eemail)
+          {userFound=true;}
+          else{userFound=false;}
+        });
+
+        if(userFound==true)
+        {
+          Get.snackbar("Error", "User already existing",colorText: Colors.red);
+        }
+        else
+        {// to validate password
+          final validatePassword=check_password(epwd);
+          if(validatePassword==true){
+            final newUser=User(email: eemail, password: epwd);
+            await HiveDb.instance.addUser(newUser);
+            Get.to(HiveLogin());
+            Get.snackbar("Success", "Registration Success",colorText: Colors.green);
+          }
+        }
+      }
+
+      else{
+        Get.snackbar("Error", "Enter a valid email id",colorText: Colors.red);
+      }
+
+    }
+    else{
+Get.snackbar("Error", "Fields must not be empty",colorText: Colors.red);
+    }
+  }
+
+  bool check_password(String epwd)
+  {
+    if(epwd.length<6){
+      Get.snackbar("Error","password length error");
+      return false;
+    }
+    else return true;
+  }
+}
--- lib/storage/hive/hive_crud/hive_crud.dart
+++ lib/storage/hive/hive_crud/hive_crud.dart
@@ -1,5 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:hive/hive.dart';
+import 'package:hive_flutter/adapters.dart';
+
+void main() async {
+  await Hive.initFlutter();
+  await Hive.openBox('todo');
+  runApp(MaterialApp(
+    home: HiveCRUD(),
+  ));
+}
 
 class HiveCRUD extends StatefulWidget {
   @override
@@ -8,97 +17,141 @@ class HiveCRUD extends StatefulWidget {
 
 class _HiveCRUDState extends State<HiveCRUD> {
   List<Map<String, dynamic>> task = [];
-final tname_ctrl=TextEditingController();
-final tcont_ctrl=TextEditingController();
-final todobox=Hive.box('todo');
-@override
+  final tname_ctrl = TextEditingController();
+  final tcont_ctrl = TextEditingController();
+  final todobox = Hive.box('todo');
+
+  @override
   void initState() {
-   read_refresh_task();
+    read_refresh_task();
     super.initState();
   }
+
   read_refresh_task() {
-  // fetch all the keys from hive in ascending order
-  // key - individual itemkeys will be available at key{
-  final task_from_hive=todobox.keys.map((key){
-    final task_from_key=todobox.get(key);
-    return{
-      'id':key,
-      'name':task_from_key['task_name'],
-      'content':task_from_key['task_content']
-    };
-  }).toList();
-  setState(() {
-    task=task_from_hive;
-  });
- }
+    // fetch all the keys from hive in ascending order
+    // key - individual itemkeys will be available at key{
+    final task_from_hive = todobox.keys.map((key) {
+      final task_from_key = todobox.get(key);
+      return {
+        'id': key,
+        'name': task_from_key['task_name'],
+        'content': task_from_key['task_content']
+      };
+    }).toList();
+    setState(() {
+      task = task_from_hive.reversed.toList();
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("TODO"),
-      ),
-      body: task.isEmpty
-          ? Center(child: CircularProgressIndicator())
-          : ListView.builder(
-          itemCount:task.length,
-          itemBuilder: (context, index) {
-              return Card(
-                child: ListTile(
-                  leading: Icon(Icons.task),
-                  title: Text(task[index]['name']),
-                  subtitle: Text(task[index]['content']),
-                  trailing: Wrap(
-                    children: [
-                      IconButton(onPressed: () {}, icon: Icon(Icons.edit)),
-                      IconButton(onPressed: () {}, icon: Icon(Icons.delete))
-                    ],
-                  ),
-                ),
-              );
-            }),
-      floatingActionButton: FloatingActionButton(onPressed: (){create_edit_task(null);},child: Icon(Icons.add),)
-    );
+        appBar: AppBar(
+          title: Text("TODO"),
+        ),
+        body: task.isEmpty
+            ? Center(child: CircularProgressIndicator())
+            : ListView.builder(
+                itemCount: task.length,
+                itemBuilder: (context, index) {
+                  return Card(
+                    child: ListTile(
+                      leading: Icon(Icons.task),
+                      title: Text(task[index]['name']),
+                      subtitle: Text(task[index]['content']),
+                      trailing: Wrap(
+                        children: [
+                          IconButton(
+                              onPressed: () {
+                                create_edit_task(task[index]['id']);
+                              },
+                              icon: Icon(Icons.edit)),
+                          IconButton(
+                              onPressed: () {
+                                delete_task(task[index]['id']);
+                              },
+                              icon: Icon(Icons.delete))
+                        ],
+                      ),
+                    ),
+                  );
+                }),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            create_edit_task(null);
+          },
+          child: Icon(Icons.add),
+        ));
   }
 
   void create_edit_task(int? itemkey) {
-
+    if (itemkey != null) {
+      final selected_task =
+          task.firstWhere((element) => element['id'] == itemkey);
+      tname_ctrl.text = selected_task['name'];
+      tcont_ctrl.text = selected_task['content'];
+    }
     showModalBottomSheet(
-      elevation: 5,
+        elevation: 5,
         isScrollControlled: true,
-        context: context, builder: (context){return Container(
-      padding:EdgeInsets.only(
-        left: 15,
-        right: 15,
-        top: 15,
-        bottom: MediaQuery.of(context).viewInsets.bottom+50,
-      ),
-      child: Column(mainAxisSize: MainAxisSize.min,
-        children: [
-        TextField(
-          controller: tname_ctrl,
-          decoration: InputDecoration(hintText: "Task Name"),
-        ),
-        TextField(
-          controller: tcont_ctrl,
-          decoration: InputDecoration(hintText: "Task Content"),
-        ),
-        ElevatedButton(onPressed: (){
-          if(itemkey==null){
-            create_task(
-                {'task_name':tname_ctrl.text.trim(),
-              'task_content':tcont_ctrl.text.trim()}
-            );
-          }
-        }, child:Text(itemkey==null?"Create Task":"Edit Task"))
-      ],),
-    );});
+        context: context,
+        builder: (context) {
+          return Container(
+            padding: EdgeInsets.only(
+              left: 15,
+              right: 15,
+              top: 15,
+              bottom: MediaQuery.of(context).viewInsets.bottom + 50,
+            ),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                TextField(
+                  controller: tname_ctrl,
+                  decoration: InputDecoration(hintText: "Task Name"),
+                ),
+                TextField(
+                  controller: tcont_ctrl,
+                  decoration: InputDecoration(hintText: "Task Content"),
+                ),
+                ElevatedButton(
+                    onPressed: () {
+                      if (itemkey == null) {
+                        create_task({
+                          'task_name': tname_ctrl.text.trim(),
+                          'task_content': tcont_ctrl.text.trim()
+                        });
+                      }
+                      if (itemkey != null) {
+                        edit_task(itemkey, {
+                          'task_name': tname_ctrl.text.trim(),
+                          'task_content': tcont_ctrl.text.trim()
+                        });
+                      }
+
+                      tname_ctrl.text = '';
+                      tcont_ctrl.text = '';
+                      Navigator.pop(context);
+                    },
+                    child: Text(itemkey == null ? "Create Task" : "Edit Task"))
+              ],
+            ),
+          );
+        });
   }
 
- Future<void>  create_task(Map<String, String> newtask)async {
+  Future<void> create_task(Map<String, String> newtask) async {
     await todobox.add(newtask);
+    read_refresh_task();
   }
 
+  Future<void> edit_task(int itemkey, Map<String, String> utask) async {
+    await todobox.put(itemkey, utask);
+    read_refresh_task();
+  }
 
-
+  Future<void> delete_task(int itemkey) async {
+    await todobox.delete(itemkey);
+    read_refresh_task();
+  }
 }
